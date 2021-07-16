a5d0459baf75633ec0274f368988fab8c75e8d2d
Andy <andy@workshopshed.com>
2021-07-16 21:00:56 +0100

    Serial hardware
    Added the serial keypad and LCD based on hobbytronics.co.uk/i2clcd-backpack-v2

--- Lockbox/Lockbox.ino
+++ Lockbox/Lockbox.ino
@@ -1,5 +1,11 @@
 #include "Delay.h"  
-  
+#include <SoftwareSerial.h>
+
+/* 
+ * RX is digital pin 2 - Connect to the keypad
+ * TX is digital pin 3 - Connect to the data pin on the screen
+ */
+
 NonBlockDelay mainTimer; 
 NonBlockDelay displayTimer;
 NonBlockDelay tickTimer;
@@ -25,26 +31,76 @@ String const keyCode_2 = "1564#";
 String const keyCode_3 = "7132#";
 String const keyCode_4 = "3684#";
 
+SoftwareSerial keyDisplay(2, 3); // RX, TX
+
 void setup() {
-  Serial.begin(9600);
+  keyDisplay.begin(9600);
+  
+   // Set display 20x4  
+  keyDisplay.write(5);
+  keyDisplay.write(4);
+  keyDisplay.write(20);
+  keyDisplay.write(0xFF); 
+  
+  // Set backlight on bright
+  keyDisplay.write(7);
+  keyDisplay.write(250);  
+  keyDisplay.write(0xFF);   
+  
+  clearDisplay();
+  
   StartTimer();
   boxState = ARMED_0;
   keyCode = keyCode_Init;
-  Serial.println("Armed");
-  Serial.print("State:");
-  Serial.println(boxState,DEC);
+
+  writeState("Armed");
+}
+
+void clearDisplay() {
+  keyDisplay.write(4);
+  keyDisplay.write(0xFF);   
+  setPosDisplay(1,1);
+}
+
+void writeDisplay(char *msg) {
+  keyDisplay.write(1);
+  keyDisplay.print(msg);
+  keyDisplay.write(0xFF);  
+}
+
+void writeState(char *msg) {
+    setPosDisplay(1,1);
+    writeDisplay(msg);
+    setPosDisplay(2,1);
+    writeDisplay("State:");
+    setPosDisplay(2,8);
+    char buffer [3];
+    itoa (boxState,buffer,10);
+    writeDisplay(buffer);
+}
+
+void setPosDisplay(int row, int col) {
+  // Move to line 2 column 1
+  mySerial.write(2);
+  mySerial.write(row);
+  mySerial.write(col);  
+  mySerial.write(0xFF); 
+ 
 }
 
 void loop() {
   CheckKeys();
   UpdateStatus();
   UpdateDisplay();
+  //Display counter
+  //Display LEDs  
 }
 
 void CheckKeys() {
   // Using Serial to temporarily to pretend to be a keypad
-  if (Serial.available() > 0) {
-    String keys = Serial.readString();
+  // Might need to do some tuning here. e.g. available() > 4
+  if (keyDisplay.available() > 0) {
+    String keys = keyDisplay.readString();
     if (keys.substring(4,5) == "#") {
         keyCode = keys.substring(0,5);
     }
@@ -63,7 +119,7 @@ void UpdateStatus() {
   }
   
   if (mainTimer.OutofTime()) {
-    Serial.println("BOOM!");
+    writeDisplay("BOOM!");
     boxState = BOOM;
     return;
   }
@@ -99,9 +155,7 @@ void UpdateStatus() {
 void CodeWrong() {
     // Speed up if wrong code entered
     displayTimer.CountDown(1000 * 3);
-    Serial.println("WRONG CODE");
-    Serial.print("State:");
-    Serial.println(boxState,DEC);
+    writeState("WRONG CODE ");
     mainTimer.Skip(1000 * 30);
     keyCode = keyCode_Init;
     tickRate = tickRate * 0.8;
@@ -109,9 +163,7 @@ void CodeWrong() {
 
 void CodeAccepted() {
     displayTimer.CountDown(1000 * 3);
-    Serial.println("CODE ACCEPTED");
-    Serial.print("State:");
-    Serial.println(boxState,DEC);
+    writeState("CODE ACCEPTED");
     keyCode = keyCode_Init;
 }
 
@@ -119,13 +171,13 @@ void UpdateDisplay() {
   //Serial.println(mainTimer.RemainingTime());
   
   if (displayTimer.OutofTime()) {
-    //Clear screen
+     clearDisplay();
   }
 
   if (tickTimer.OutofTime()) {
-    Serial.println("TICK");
+    clearDisplay();
+    writeDisplay("TICK");
     tickTimer.CountDown(tickRate);
+    displayTimer.CountDown(1000 * 3);
   }
-  //Display counter
-  //Display LEDs
 }
